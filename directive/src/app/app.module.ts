829a1a6fba905c6964ad32e572104348689a6d31
puja lakhe <lakhepuza924@gmail.com>
2024-04-22 16:08:22 +0545

    path are reorganized

--- directive/src/app/app.module.ts
+++ directive/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as components from './import';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    components.AttributeDirectiveComponent,
+    components.StructuralDirectiveComponent
   ],
   imports: [
     BrowserModule,
