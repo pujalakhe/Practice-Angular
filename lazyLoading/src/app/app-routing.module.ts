93af51b82a9f712e6c6584ef4a9018fe54b95bc8
puja lakhe <lakhepuza924@gmail.com>
2024-05-05 18:03:26 +0545

    changes made due to new components

--- lazyLoading/src/app/app-routing.module.ts
+++ lazyLoading/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'analytics', loadChildren: () => import('./analytics/analytics.module').then(m => m.AnalyticsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lazyLoading/src/app/app.module.ts
+++ lazyLoading/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Router } from 'express';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideClientHydration()
