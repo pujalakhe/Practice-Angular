fe263a824d95bb7410791d6b0d950873ff367a11
puja lakhe <lakhepuza924@gmail.com>
2024-05-05 18:04:15 +0545

    auth updated

--- lazyLoading/src/app/auth/auth-routing.module.ts
+++ lazyLoading/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
-
-const routes: Routes = [{ path: '', component: AuthComponent }];
+import { LoginComponent } from './components/login/login.component';
+const routes: Routes = [{ path: '', component: AuthComponent },
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
