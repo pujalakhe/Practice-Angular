490975a54d0edd27f59234bc476eafe659637dbe
puja lakhe <lakhepuza924@gmail.com>
2024-05-07 14:04:11 +0545

    modified

--- lazyLoading/src/app/app-routing.module.ts
+++ lazyLoading/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
+  // {path:'about',component:AboutComponent},
+  {path:'about',loadComponent:()=>import('./components/about/about.component').then(c=>c.AboutComponent)},
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'analytics', loadChildren: () => import('./analytics/analytics.module').then(m => m.AnalyticsModule) }
 ];
--- lazyLoading/src/app/app.module.ts
+++ lazyLoading/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { Router } from 'express';
+import { AboutComponent } from './components/about/about.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterLink,RouterOutlet
   ],
   providers: [
     provideClientHydration()
