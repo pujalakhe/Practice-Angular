2905a1a98d96ab0672a6b05f6a5a1dd726ac525b
puja lakhe <lakhepuza924@gmail.com>
2024-05-14 14:38:39 +0545

    login component

--- guard/src/app/components/login/login.component.ts
+++ guard/src/app/components/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component,inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username!:string;
+  userpassword!:string;
+  isLogInClicked:boolean=false;
+  router:Router=inject(Router);
+  userLoggedIn:AuthService=inject(AuthService)
+  onLoggedIn(username:string,userpassword:string)
+  {
+    if(username=="admin" && userpassword=='1')
+      {
+        alert(` ${username} logged in`);
+        this.userLoggedIn.isLoggedIn=true;
+        return this.router.navigate(['/product']);
+      }
+      else{
+        alert('inavlid credentials');
+        return false;
+      }
+  }
+  //canDeactive
+
 
 }
