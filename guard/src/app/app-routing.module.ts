467cfe28777068b57a2a0ee7241796cf59722d5a
puja lakhe <lakhepuza924@gmail.com>
2024-05-14 20:39:51 +0545

    router events tracked

--- guard/src/app/app-routing.module.ts
+++ guard/src/app/app-routing.module.ts
@@ -4,29 +4,41 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { PrdouctComponent } from './components/prdouct/prdouct.component';
 import { authGuard } from './guard/auth.guard';
-import { activeChildGuard, canDeactivate } from './guard/active-child.guard';
+import {
+  activeChildGuard,
+  canDeactivate,
+  resolveGuard,
+} from './guard/active-child.guard';
 import { LoginComponent } from './components/login/login.component';
 import { BuyComponent } from './components/prdouct/buy/buy.component';
 import { AddToCartComponent } from './components/prdouct/add-to-cart/add-to-cart.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'home',component:HomeComponent},
-  {path:'about',component:AboutComponent},
-  {path:'product',component:PrdouctComponent,canActivate:[activeChildGuard],
-    children:
-    [{
-      path:'buy',component:BuyComponent,canDeactivate:[canDeactivate]
-    },
-    {
-      path:'add-to-cart',component:AddToCartComponent
-    }]
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  {
+    path: 'product',
+    component: PrdouctComponent,
+    canActivate: [activeChildGuard],
+    resolve: { product: resolveGuard },
+    children: [
+      {
+        path: 'buy',
+        component: BuyComponent,
+        canDeactivate: [canDeactivate],
+      },
+      {
+        path: 'add-to-cart',
+        component: AddToCartComponent,
+      },
+    ],
   },
-  {path:'login',component:LoginComponent}
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
