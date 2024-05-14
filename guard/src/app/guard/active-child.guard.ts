0d098d5986a8e149f80ba8bff77c93b5b00ee2d7
puja lakhe <lakhepuza924@gmail.com>
2024-05-14 14:38:08 +0545

    add canDeactive guard logic

--- guard/src/app/guard/active-child.guard.ts
+++ guard/src/app/guard/active-child.guard.ts
@@ -1,6 +1,7 @@
 import { ActivatedRouteSnapshot, CanActivateFn,Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject, } from '@angular/core';
+import { BuyComponent } from '../components/prdouct/buy/buy.component';
 export const authGuard = 
   () => {
   // let authService = inject(AuthService);
@@ -23,5 +24,8 @@ export const authGuard =
 };
 export const activeChildGuard = ( ) => {
   return authGuard();
+};
+export const canDeactivate = (component:BuyComponent) => {
+  return component.canExit();
 };
 
