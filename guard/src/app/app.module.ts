ab6a4bc42a062b1b165eeffd1fefc0ece106c8dd
puja lakhe <lakhepuza924@gmail.com>
2024-05-14 14:28:20 +0545

    add buy and addToCart componenet

--- guard/src/app/app.module.ts
+++ guard/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PrdouctComponent } from './components/prdouct/prdouct.component';
 import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
+import { BuyComponent } from './components/prdouct/buy/buy.component';
+import { AddToCartComponent } from './components/prdouct/add-to-cart/add-to-cart.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,14 @@ import { LoginComponent } from './components/login/login.component';
     HomeComponent,
     PrdouctComponent,
     AboutComponent,
-    LoginComponent
+    LoginComponent,
+    BuyComponent,
+    AddToCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
