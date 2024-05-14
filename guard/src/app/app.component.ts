7c31e6d74ac70c8d342912f9a8714b3a50bd4913
puja lakhe <lakhepuza924@gmail.com>
2024-05-14 20:38:54 +0545

    logic to display loader

--- guard/src/app/app.component.ts
+++ guard/src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component,OnInit,inject } from '@angular/core';
-import { AuthService } from './services/auth.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, Event } from '@angular/router';
+
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   title = 'guard';
+  showLoader: boolean = false;
+  constructor(public router: Router) {}
+  ngOnInit() {
+    this.router.events.subscribe((RouterEvent: Event) => {
+      if (RouterEvent instanceof NavigationStart) {
+        this.showLoader = true;
+      }
+      if (
+        RouterEvent instanceof NavigationEnd ||
+        RouterEvent instanceof NavigationCancel ||
+        RouterEvent instanceof NavigationError
+      ) {
+        this.showLoader = false;
+      }
+    });
+  }
 }
