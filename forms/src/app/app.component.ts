179b4174b7daad46bc7f90b00c9ec9c3c192820a
puja lakhe <lakhepuza924@gmail.com>
2024-05-19 11:55:22 +0545

    formarray imported

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -3,14 +3,28 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { FormarrayComponent } from './formarray/formarray.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,TemplateDrivenFormComponent,RouterOutlet,ReactiveFormComponent,RouterLink,RouterLinkActive],
+  imports: [
+    CommonModule,
+    TemplateDrivenFormComponent,
+    RouterOutlet,
+    ReactiveFormComponent,
+    RouterLink,
+    RouterLinkActive,
+    FormBuilderComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    FormarrayComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'forms';
-
 }
--- forms/src/app/app.routes.ts
+++ forms/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
 
 export const routes: Routes = [
-    {path:'reactive',component:ReactiveFormComponent},
-    {path:'template',component:TemplateDrivenFormComponent}
+  { path: 'reactive', component: ReactiveFormComponent },
+  { path: 'template', component: TemplateDrivenFormComponent },
+  { path: 'builder', component: FormBuilderComponent },
 ];
