d2964f8601486b78140410d74ff250ef0af61d10
puja lakhe <lakhepuza924@gmail.com>
2024-05-19 18:08:47 +0545

    custom validation rood add

--- forms/src/app/app.routes.ts
+++ forms/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { FormarrayComponent } from './formarray/formarray.component';
+import { CustomValidationComponent } from './custom-validation/custom-validation.component';
 
 export const routes: Routes = [
   { path: 'reactive', component: ReactiveFormComponent },
   { path: 'template', component: TemplateDrivenFormComponent },
   { path: 'builder', component: FormBuilderComponent },
+  { path: 'formarray', component: FormarrayComponent },
+  { path: 'customValidation', component: CustomValidationComponent },
 ];
